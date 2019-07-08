3fb5a4052797e9001d0a3cf30a14b109b018b401
xiaomangguo <37840914+chenpenfree@users.noreply.github.com>
2019-07-08 19:28:39 +0800

    Update quick_sort.py

--- sort_algorithms/quick_sort.py
+++ sort_algorithms/quick_sort.py
@@ -21,9 +21,20 @@ def sort(var_list: typing.List[int], start_position, end_position):
     
     pivot_index = partition(var_list, start_position, end_position)  # 支点索引
     
-    sort(var_list, start_position, pivot_index)
+    sort(var_list, start_position, pivot_index - 1)
     sort(var_list, pivot_index + 1, end_position)
     
 def partition(var_list: typing.List[int], start_position, end_position):
-    """获取分割点"""
+    """获取分割点：单边扫描，选取最后一个元素为基点"""
     
+    pivot_value = var_list[end_position]
+    mark = start_position
+    
+    for i in range(start_position, end_position):
+        if var_list[i] < pivot_value:
+            var_list[i], var_list[mark] = var_list[mark], var_list[i]
+            mark += 1
+            
+    var_list[mark], var_list[end_position] = var_list[end_position], var_list[mark]
+    
+    return mark
